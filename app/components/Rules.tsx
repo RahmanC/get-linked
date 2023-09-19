7cd0300d59e9471ceeb87c0673706add343a58b6
rhammy <afaraeturahman@gmail.com>
2023-09-19 18:21:00 +0100

    rules

--- app/components/Rules.tsx
+++ app/components/Rules.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 
-const Introduction = () => {
+const Rules = () => {
   return (
-    <section className="flex flex-col gap-5 md:gap-24 md:flex-row items-center justify-center border-b border-t border-[#ffffff] border-opacity-[18%] px-[2.5rem] md:px-auto py-[1.8125rem] md:py-[3.875rem]">
-      <img src="/big-idea.svg" alt="getLinked" />
-      <div>
-        <p className="flex flex-col items-center md:items-start text-[1.25rem] md:text-[2rem] font-[700]">
-          Introduction to getlinked{" "}
-          <span className="text-[#D434FE]">tech Hackathon 1.0</span>
+    <section className="flex flex-col gap-5 md:gap-24 md:flex-row-reverse items-center justify-center border-b border-[#ffffff] border-opacity-[18%] px-[2.5rem] md:px-[11.5rem] py-[1.8125rem] md:py-[3.875rem]">
+      <img src="/guidelines.svg" alt="getLinked" />
+      <div className="md:pl-[3rem]">
+        <p className="flex flex-col items-center md:items-start text-[1.25rem] md:text-[2rem] font-[700] ">
+          Rules and <span className="text-[#D434FE]">Guidelines</span>
         </p>
         <p className="md:text-[0.875rem] text-[0.8125rem] text-center md:text-left  mt-4 md:w-[33rem]">
           Our tech hackathon is a melting pot of visionaries, and its purpose is
@@ -22,4 +21,4 @@ const Introduction = () => {
   );
 };
 
-export default Introduction;
+export default Rules;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import Introduction from "@/components/Introduction";
+import Rules from "@/components/Rules";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <Introduction />
+      <Rules />
     </div>
   );
 }
